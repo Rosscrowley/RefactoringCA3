7ca0b73ab096437a32ef2fe7edaedfc4e8be75a0
Rosscrowley <91560583+Rosscrowley@users.noreply.github.com>
2024-02-28 16:29:14 +0000

    Initial refactoring of EmployeeSummaryDialog

--- src/EmployeeSummaryDialog.java
+++ src/EmployeeSummaryDialog.java
@@ -11,6 +11,7 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
+import java.util.Arrays;
 import java.util.Vector;
 
 import javax.swing.BorderFactory;
@@ -23,105 +24,115 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 import net.miginfocom.swing.MigLayout;
 
 public class EmployeeSummaryDialog extends JDialog implements ActionListener {
     // vector with all Employees details
     Vector<Vector<Object>> allEmployees;
-    JButton back;
+    JButton backButton;
+
+    private static final int WIDTH = 850;
+    private static final int HEIGHT = 500;
+    private static final int LOCATION_X = 350;
+    private static final int LOCATION_Y = 250;
+
+    private static final Dimension TABLE_VIEWPORT_SIZE = new Dimension(800, 300);
+    private static final String[] HEADER_NAMES = { "ID", "PPS Number", "Surname", "First Name", "Gender", "Department", "Salary", "Full Time" };
+    private static final int[] COLUMN_WIDTHS = { 15, 100, 120, 120, 50, 120, 80, 80 };
+
 
     public EmployeeSummaryDialog(Vector<Vector<Object>> allEmployees) {
+        this.allEmployees = allEmployees;
+        initializeDialog();
+    }
+    private void initializeDialog() {
         setTitle("Employee Summary");
         setModal(true);
-        this.allEmployees = allEmployees;
-
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-        JScrollPane scrollPane = new JScrollPane(summaryPane());
-        setContentPane(scrollPane);
-
-        setSize(850, 500);
-        setLocation(350, 250);
+        addComponentsToPane();
+        setSize(WIDTH, HEIGHT);
+        setLocation(LOCATION_X, LOCATION_Y);
         setVisible(true);
-
     }
-    // initialise container
-    public Container summaryPane() {
-        JPanel summaryDialog = new JPanel(new MigLayout());
+    private void addComponentsToPane() {
+        JScrollPane scrollPane = new JScrollPane(EmployeeSummaryPane());
+        setContentPane(scrollPane);
+    }
+    private Container EmployeeSummaryPane() {
+         JPanel summaryDialog = new JPanel(new MigLayout());
+         JPanel buttonPanel = setupButtonPanel();
+
+         JTable employeeTable = setupEmployeeTable();
+         JScrollPane scrollPane = new JScrollPane(employeeTable);
+         scrollPane.setBorder(BorderFactory.createTitledBorder("Employee Details"));
+         scrollPane.setPreferredSize(TABLE_VIEWPORT_SIZE);
+
+         summaryDialog.add(buttonPanel, "growx, pushx, wrap");
+         summaryDialog.add(scrollPane, "growx, pushx, wrap");
+
+         return summaryDialog;
+    }
+    private JPanel setupButtonPanel() {
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        JTable employeeTable;
-        DefaultTableModel tableModel;
-        // column center alignment
-        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
-        // column left alignment
-        DefaultTableCellRenderer leftRenderer = new DefaultTableCellRenderer();
-        Vector<String> header = new Vector<String>();
-        // header names
-        String[] headerName = { "ID", "PPS Number", "Surname", "First Name", "Gender", "Department", "Salary",
-                "Full Time" };
-        // column widths
-        int[] colWidth = { 15, 100, 120, 120, 50, 120, 80, 80 };
-        centerRenderer.setHorizontalAlignment(JLabel.CENTER);
-        leftRenderer.setHorizontalAlignment(JLabel.LEFT);
-        // add headers
-        for (int i = 0; i < headerName.length; i++) {
-            header.addElement(headerName[i]);
-        }// end for
-        // construnct table and choose table model for each column
-        tableModel = new DefaultTableModel(this.allEmployees, header) {
-            public Class getColumnClass(int c) {
-                switch (c) {
-                    case 0:
-                        return Integer.class;
-                    case 4:
-                        return Character.class;
-                    case 6:
-                        return Double.class;
-                    case 7:
-                        return Boolean.class;
-                    default:
-                        return String.class;
-                }// end switch
-            }// end getColumnClass
-        };
-
-        employeeTable = new JTable(tableModel);
-        // add header names to table
-        for (int i = 0; i < employeeTable.getColumnCount(); i++) {
-            employeeTable.getColumn(headerName[i]).setMinWidth(colWidth[i]);
-        }// end for
-        // set alignments
-        employeeTable.getColumnModel().getColumn(0).setCellRenderer(leftRenderer);
-        employeeTable.getColumnModel().getColumn(4).setCellRenderer(centerRenderer);
-        employeeTable.getColumnModel().getColumn(6).setCellRenderer(new DecimalFormatRenderer());
-
+        backButton = new JButton("Back");
+        backButton.addActionListener(this);
+        backButton.setToolTipText("Return to main screen");
+        buttonPanel.add(backButton);
+        return buttonPanel;
+    }
+    private JTable setupEmployeeTable() {
+        Vector<String> header = new Vector<>(Arrays.asList(HEADER_NAMES));
+        DefaultTableModel tableModel = new CustomTableModel(this.allEmployees, header);
+        JTable employeeTable = new JTable(tableModel);
+        configureTableColumns(employeeTable);
         employeeTable.setEnabled(false);
-        employeeTable.setPreferredScrollableViewportSize(new Dimension(800, (15 * employeeTable.getRowCount() + 15)));
         employeeTable.setAutoCreateRowSorter(true);
-        JScrollPane scrollPane = new JScrollPane(employeeTable);
+        return employeeTable;
+    }
+    private void configureTableColumns(JTable table) {
+        DefaultTableCellRenderer leftRenderer = new DefaultTableCellRenderer();
+        leftRenderer.setHorizontalAlignment(JLabel.LEFT);
 
-        buttonPanel.add(back = new JButton("Back"));
-        back.addActionListener(this);
-        back.setToolTipText("Return to main screen");
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment(JLabel.CENTER);
 
-        summaryDialog.add(buttonPanel,"growx, pushx, wrap");
-        summaryDialog.add(scrollPane,"growx, pushx, wrap");
-        scrollPane.setBorder(BorderFactory.createTitledBorder("Employee Details"));
-
-        return summaryDialog;
-    }// end summaryPane
+        for (int i = 0; i < table.getColumnCount(); i++) {
+            TableColumn column = table.getColumnModel().getColumn(i);
+            column.setMinWidth(COLUMN_WIDTHS[i]);
+            if (i == 0) {
+                column.setCellRenderer(leftRenderer);
+            } else if (i == 4 || i == 6) {
+                column.setCellRenderer(i == 4 ? centerRenderer : new DecimalFormatRenderer());
+            }
+        }
+    }
+    private class CustomTableModel extends DefaultTableModel {
+        public CustomTableModel(Vector<Vector<Object>> data, Vector<String> header) {
+            super(data, header);
+        }
+        @Override
+        public Class<?> getColumnClass(int columnIndex) {
+            return switch (columnIndex) {
+                case 0 -> Integer.class;
+                case 4 -> Character.class;
+                case 6 -> Double.class;
+                case 7 -> Boolean.class;
+                default -> String.class;
+            };
+        }
+    }
 
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == back){
+        if (e.getSource() == backButton){
             dispose();
         }
-
     }
     // format for salary column
     static class DecimalFormatRenderer extends DefaultTableCellRenderer {
         private static final DecimalFormat format = new DecimalFormat(
-                "\u20ac ###,###,##0.00" );
+                "€ ###,###,##0.00" );
 
         public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
                                                        int row, int column) {
