679212a99d8e3876664c0ffe9a08cca747a9f6d9
Rosscrowley <91560583+Rosscrowley@users.noreply.github.com>
2024-03-03 15:20:49 +0000

    SearchByIdDialog and SearchBySurnameDialog changes to use new abstract SearchDialog class

--- src/SearchByIdDialog.java
+++ src/SearchByIdDialog.java
@@ -21,57 +21,13 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.border.EtchedBorder;
 
-public class SearchByIdDialog extends JDialog implements ActionListener {
-    EmployeeDetails parent;
-    JButton search, cancel;
-    JTextField searchField;
-    // constructor for SearchByIdDialog
+public class SearchByIdDialog extends SearchDialog  {
+
     public SearchByIdDialog(EmployeeDetails parent) {
-        setTitle("Search by Surname");
-        setModal(true);
-        this.parent = parent;
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        super(parent, "Search by ID", "ID");
+    }
 
-        JScrollPane scrollPane = new JScrollPane(searchPane());
-        setContentPane(scrollPane);
-
-        getRootPane().setDefaultButton(search);
-
-        setSize(500, 190);
-        setLocation(350, 250);
-        setVisible(true);
-    }// end SearchByIdDialog
-
-    // initialize search container
-    public Container searchPane() {
-        JPanel searchPanel = new JPanel(new GridLayout(3, 1));
-        JPanel textPanel = new JPanel();
-        JPanel buttonPanel = new JPanel();
-        JLabel searchLabel;
-
-        searchPanel.add(new JLabel("Search by ID"));
-
-        textPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
-        textPanel.add(searchLabel = new JLabel("Enter ID:"));
-        searchLabel.setFont(this.parent.font1);
-        textPanel.add(searchField = new JTextField(20));
-        searchField.setFont(this.parent.font1);
-        searchField.setDocument(new JTextFieldLimit(20));
-
-        buttonPanel.add(search = new JButton("Search"));
-        search.addActionListener(this);
-        search.requestFocus();
-
-        buttonPanel.add(cancel = new JButton("Cancel"));
-        cancel.addActionListener(this);
-
-        searchPanel.add(textPanel);
-        searchPanel.add(buttonPanel);
-
-        return searchPanel;
-    }// end searchPane
-
-    // action listener for save and cancel button
+    @Override
     public void actionPerformed(ActionEvent e) {
         // if option search, search for Employee
         if (e.getSource() == search) {
@@ -93,4 +49,5 @@ public class SearchByIdDialog extends JDialog implements ActionListener {
         else if (e.getSource() == cancel)
             dispose();
     }// end actionPerformed
+
 }// end class searchByIdDialog
